0bfc64808d09284112949101cbce3b32fa14c309
Xiang Li <xiangli5699@gmail.com>
2024-02-08 12:33:19 -0500

    Variant count by patient

--- scripts/download_files.py
+++ scripts/download_files.py
@@ -7,7 +7,7 @@ import json
 url = "https://api.github.com/repos/cBioPortal/datahub/git/trees/master?recursive=1"
 
 # Target path to save the files
-target_path = "./files/"
+target_path = "./files/tcga"
 
 # Send a GET request to the GitHub API
 response = requests.get(url)
@@ -22,21 +22,31 @@ tcga_dirs = [dir for dir in directories if dir['path'].endswith("tcga_pan_can_at
 # Print the count of such directories
 print(f"Found {len(tcga_dirs)} directories.")
 
-# Go to each directory and download "data_mutations.txt"
+# Go to each directory "data_mutations.txt" and "data_clinical_sample.txt"
 for directory in tcga_dirs:
     # Construct the raw URL of the file
     print(directory['path'][7:])
-    file_url = f"https://media.githubusercontent.com/media/cBioPortal/datahub/master/{directory['path']}/data_mutations.txt"
+    mutation_file_url = f"https://media.githubusercontent.com/media/cBioPortal/datahub/master/{directory['path']}/data_mutations.txt"
+    clinical_file_url = f"https://media.githubusercontent.com/media/cBioPortal/datahub/master/{directory['path']}/data_clinical_sample.txt"
     
-    # Send a GET request to the raw URL
-    file_response = requests.get(file_url)
+    mutation_file_response = requests.get(mutation_file_url)
+    clinical_file_response = requests.get(clinical_file_url)
 
-    # If the file exists, the status code of the response will be 200
-    if file_response.status_code == 200:
-        # Save the file with the directory's name
-        filename = os.path.join(target_path, directory['path'].split('/')[-1] + ".txt")
-        with open(filename, 'wb') as f:
-            f.write(file_response.content)
-        print(f"Downloaded file to {filename}")
+    if mutation_file_response.status_code == 200:
+        mutation_filename = os.path.join(target_path, directory['path'].split('/')[-1] + "_mutations.txt")
+        with open(mutation_filename, 'wb') as f:
+            f.write(mutation_file_response.content)
+        print(f"Downloaded file to {mutation_filename}")
+    else:
+        print(f"No 'data_mutations.txt' found in {directory['path']}")
+    
+    if clinical_file_response.status_code == 200:
+        clinical_filename = os.path.join(target_path, directory['path'].split('/')[-1] + "_clinical_samples.txt")
+        with open(clinical_filename, 'wb') as f:
+            lines = clinical_file_response.content.splitlines()
+            for line in lines[4:]:
+                f.write(line + b'\n')
+
+        print(f"Downloaded file to {clinical_filename}")
     else:
         print(f"No 'data_mutations.txt' found in {directory['path']}")
--- scripts/variant_count.py
+++ scripts/variant_count.py
@@ -1,48 +1,76 @@
-import pandas as pd
 import json
-import glob
+import os
+import pandas as pd
+from typing import List
 
-# Get a list of all TXT files in the mskimpact and tcga folders
-mskimpact_files = glob.glob('./files/mskimpact/*.txt')
-tcga_files = glob.glob('./files/tcga/*.txt')
 
-# Create maps for variants count
-mskimpactCounts = {'germlineVariantsCount': {}, 'somaticVariantsCount': {}, 'unknownVariantsCount': {}, 'totalSampleCount': 0, 'sampleCountByGene': {}}
-tcgaCounts = {'germlineVariantsCount': {}, 'somaticVariantsCount': {}, 'unknownVariantsCount': {}, 'totalSampleCount': 0, 'sampleCountByGene': {}}
+# Download files first
+# There will be internal MSK-IMAPCT, GENIE v15 public cohort, and TCGA Pan-Cancer Atlas (32 cohorts)
+# Each one contains xxx_mutations.txt and xxx_clinical_samples.txt
+# TCGA Pan-Cancer data can be downloaded by running download_files.py. Files are stored in /files/tcga
+# MSK-IMAPCT data can be downloaded from https://github.mskcc.org/cdsi/msk-impact.git in msk_solid_heme folder. Files are stored in /files/mskimpact
+# GENIE data can be downloaded from https://www.synapse.org/#!Synapse:syn53210170. Files are stored in /files/genie
 
-# Read the JSON file
+class StudyInfo:
+    def __init__(self, panel_name: str, panel_list: List[str]):
+        self.panel_name = panel_name
+        self.panel_list = panel_list
+
+def find_file_pairs(folder_path: str):
+    # Get all files in the directory
+    files = os.listdir(folder_path)
+    mutation_files = [f for f in files if f.endswith('_mutations.txt')]
+    clinical_files = [f for f in files if f.endswith('_clinical_samples.txt')]
+
+    # Find pairs of files that have the same prefix
+    pairs = []
+    for m_file in mutation_files:
+        prefix = m_file.replace('_mutations.txt', '')
+        for c_file in clinical_files:
+            if c_file.startswith(prefix):
+                pairs.append((m_file, c_file))
+
+    return pairs
+
 with open('../VUEs.json', 'r') as f:
     data = json.load(f)
 
-# Create a set from the JSON data
-jsonSet = {vue['genomicLocation'] for vueSet in data for vue in vueSet['revisedProteinEffects']}
+def updateCounts(mutation_file: str, clinical_file: str, study_id: str, study_info: StudyInfo, multiple_study: bool, first_study: bool):
+    jsonSet = {vue['genomicLocation'] for vueSet in data for vue in vueSet['revisedProteinEffects']}
 
-# Define a function to update the counts
-def updateCounts(files, counts):
-    # Iterate over each file
-    for file in files:
-        # Read each TXT file into a DataFrame
-        df = pd.read_csv(file, sep="\t", usecols=['Hugo_Symbol', 'Chromosome', 'Start_Position', 'End_Position', 'Reference_Allele', 'Tumor_Seq_Allele1', 'Tumor_Seq_Allele2', 'Mutation_Status', 'Tumor_Sample_Barcode'])
-        
-        print("Counting from " + file)
-        # Update the total sample count
-        counts['totalSampleCount'] += df['Tumor_Sample_Barcode'].nunique()
+    counts =  {'germlineVariantsCount': {}, 'somaticVariantsCount': {}, 'unknownVariantsCount': {}, 'totalPatientCount': 0, 'patientCountByGene': {}}
+    mutationPatientsId = set()
+    checkedPatientId = set()
+    # Read clinical sample data
+    clinical_sample_df = pd.read_csv(clinical_file, sep='\t')
 
-        # Group the DataFrame by 'Hugo_Symbol' and count unique 'Tumor_Sample_Barcode' values
-        sampleCountByGene = df.groupby('Hugo_Symbol')['Tumor_Sample_Barcode'].nunique()
-        
-        # Update the sampleCountByGene map
-        for gene, count in sampleCountByGene.iteritems():
-            counts['sampleCountByGene'][gene] = counts['sampleCountByGene'].get(gene, 0) + count
+    # Filter data by gene panel
+    if study_info is not None:
+        filtered_clinical_sample_df = clinical_sample_df[clinical_sample_df[study_info.panel_name].isin(study_info.panel_list)]
+    else:
+        filtered_clinical_sample_df = clinical_sample_df
 
-        # Iterate over the DataFrame rows
-        for index, row in df.iterrows():
-            # Generate the genomicLocationString
-            genomicLocationString = f"{row['Chromosome']},{row['Start_Position']},{row['End_Position']},{row['Reference_Allele']},{row['Tumor_Seq_Allele2']}"
-            
-            # Check if the genomicLocationString is in the jsonSet
-            if genomicLocationString in jsonSet:
-                # If it is, increment the count in the corresponding variantsCount map
+    # Count unique PATIENT_ID
+    counts['totalPatientCount'] = len(filtered_clinical_sample_df['PATIENT_ID'].unique())
+    sample_to_patient_map = dict(zip(filtered_clinical_sample_df['SAMPLE_ID'], filtered_clinical_sample_df['PATIENT_ID']))
+    mutations_df = pd.read_csv(mutation_file, sep='\t', usecols=['Hugo_Symbol', 'Chromosome', 'Start_Position', 'End_Position', 'Reference_Allele', 'Tumor_Seq_Allele1', 'Tumor_Seq_Allele2', 'Mutation_Status', 'Tumor_Sample_Barcode'])
+
+    filtered_mutations_df = mutations_df[mutations_df['Tumor_Sample_Barcode'].isin(sample_to_patient_map.keys())]
+
+    # Count sample number by gene
+    # Group the DataFrame by 'Hugo_Symbol' and count unique 'Tumor_Sample_Barcode' values
+    for gene, group in filtered_mutations_df.groupby('Hugo_Symbol'):
+        for index, row in group.iterrows():
+            patientId = sample_to_patient_map.get(row['Tumor_Sample_Barcode'])
+            if patientId and patientId not in checkedPatientId:
+                counts['patientCountByGene'][gene] = counts['patientCountByGene'].get(gene, 0) + 1
+                checkedPatientId.add(patientId)
+
+    for index, row in filtered_mutations_df.iterrows():
+        genomicLocationString = f"{row['Chromosome']},{row['Start_Position']},{row['End_Position']},{row['Reference_Allele']},{row['Tumor_Seq_Allele2']}"
+        if genomicLocationString in jsonSet:
+            if row['Tumor_Sample_Barcode'] in sample_to_patient_map and sample_to_patient_map[row['Tumor_Sample_Barcode']] not in mutationPatientsId:
+                mutationPatientsId.add(sample_to_patient_map[row['Tumor_Sample_Barcode']])
                 if row['Mutation_Status'].lower() == 'germline':
                     counts['germlineVariantsCount'][genomicLocationString] = counts['germlineVariantsCount'].get(genomicLocationString, 0) + 1
                 elif row['Mutation_Status'].lower() == 'somatic':
@@ -50,29 +78,50 @@ def updateCounts(files, counts):
                 else:
                     counts['unknownVariantsCount'][genomicLocationString] = counts['unknownVariantsCount'].get(genomicLocationString, 0) + 1
 
-# Update the counts for the mskimpact and tcga files
-updateCounts(mskimpact_files, mskimpactCounts)
-updateCounts(tcga_files, tcgaCounts)
+    # Add the count numbers to the corresponding JSON objects
+    for vueSet in data:
+        for vue in vueSet['revisedProteinEffects']:
+            if 'counts' in vue:
+                if multiple_study and study_id in vue['counts'] and not first_study:
+                    vue['counts'][study_id] = { 
+                            "germlineVariantsCount": counts["germlineVariantsCount"].get(vue['genomicLocation'], 0) + vue['counts'][study_id]['germlineVariantsCount'],
+                            "somaticVariantsCount": counts["somaticVariantsCount"].get(vue['genomicLocation'], 0) + vue['counts'][study_id]['somaticVariantsCount'],
+                            "unknownVariantsCount": counts["unknownVariantsCount"].get(vue['genomicLocation'], 0) + vue['counts'][study_id]['unknownVariantsCount'],
+                            "totalPatientCount": counts.get("totalPatientCount") + vue['counts'][study_id]['totalPatientCount'],
+                            "genePatientCount": counts['patientCountByGene'].get(vueSet['hugoGeneSymbol'], 0) + vue['counts'][study_id]['genePatientCount']
+                        }
+                else:
+                    vue['counts'][study_id] = { 
+                            "germlineVariantsCount": counts["germlineVariantsCount"].get(vue['genomicLocation'], 0),
+                            "somaticVariantsCount": counts["somaticVariantsCount"].get(vue['genomicLocation'], 0),
+                            "unknownVariantsCount": counts["unknownVariantsCount"].get(vue['genomicLocation'], 0),
+                            "totalPatientCount": counts.get("totalPatientCount"),
+                            "genePatientCount": counts['patientCountByGene'].get(vueSet['hugoGeneSymbol'], 0)
+                        }
+            else:
+                vue['counts']= { 
+                    study_id: {
+                        "germlineVariantsCount": counts["germlineVariantsCount"].get(vue['genomicLocation'], 0),
+                        "somaticVariantsCount": counts["somaticVariantsCount"].get(vue['genomicLocation'], 0),
+                        "unknownVariantsCount": counts["unknownVariantsCount"].get(vue['genomicLocation'], 0),
+                        "totalPatientCount": counts.get("totalPatientCount"),
+                        "genePatientCount": counts['patientCountByGene'].get(vueSet['hugoGeneSymbol'], 0)
+                }}
 
-# Add the count numbers to the corresponding JSON objects
-for item in data:
-    for vue in item['revisedProteinEffects']:
-        vue['counts'] = {
-            'mskimpact': {
-                "germlineVariantsCount": mskimpactCounts["germlineVariantsCount"].get(vue['genomicLocation'], 0),
-                "somaticVariantsCount": mskimpactCounts["somaticVariantsCount"].get(vue['genomicLocation'], 0),
-                "unknownVariantsCount": mskimpactCounts["unknownVariantsCount"].get(vue['genomicLocation'], 0),
-                "totalSampleCount": mskimpactCounts.get("totalSampleCount"),
-                "geneSampleCount": mskimpactCounts['sampleCountByGene'].get(item['hugoGeneSymbol'], 0)
-            },
-            'tcga': {
-                "germlineVariantsCount": tcgaCounts["germlineVariantsCount"].get(vue['genomicLocation'], 0),
-                "somaticVariantsCount": tcgaCounts["somaticVariantsCount"].get(vue['genomicLocation'], 0),
-                "unknownVariantsCount": tcgaCounts["unknownVariantsCount"].get(vue['genomicLocation'], 0),
-                "totalSampleCount": tcgaCounts.get("totalSampleCount"),
-                "geneSampleCount": tcgaCounts['sampleCountByGene'].get(item['hugoGeneSymbol'], 0)
-            }
-        }
+# Update mskimpact counts
+study_info_mskimpact = StudyInfo('GENE_PANEL', ["IMPACT341", "IMPACT410", "IMPACT468", "IMPACT505"])
+updateCounts('./files/mskimpact/mskimpact_data_mutations_extended.txt', './files/mskimpact/mskimpact_data_clinical_sample.txt', 'mskimpact', study_info_mskimpact, False, False)
+# Update genie counts
+study_info_genie = StudyInfo('SEQ_ASSAY_ID', ["MSK-IMPACT341","MSK-IMPACT410", "MSK-IMPACT468", "MSK-IMPACT505"])
+updateCounts('./files/genie/genie_data_mutations_extended.txt', './files/genie/genie_data_clinical_sample.txt', 'genie', study_info_genie, False, False)
+# Update tcga counts
+tcga_folder = './files/tcga'
+tcga_file_pairs = find_file_pairs(tcga_folder)
+for index, pair in enumerate(tcga_file_pairs):
+    mutation_file_name, clinical_file_name = pair
+    mutation_file_path = tcga_folder + '/' + mutation_file_name
+    clinical_file_path = tcga_folder + '/' + clinical_file_name
+    updateCounts(mutation_file_path, clinical_file_path, 'tcga', None, True, index == 0)
 
 # Write the updated JSON data back to the file
 with open('../VUEs.json', 'w') as f:
