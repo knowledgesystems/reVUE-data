ee0b49e45c4ac7575ecc0f008b63c3cab156c1a1
Xiang Li <xiangli5699@gmail.com>
2025-04-15 14:44:54 -0400

    Update plot and script

--- scripts/variant_count.py
+++ scripts/variant_count.py
@@ -3,6 +3,7 @@ import os
 import pandas as pd
 from typing import List, Dict, Set, Any, Tuple
 import requests
+from itertools import chain
 
 # Download files first
 # There will be internal MSK-IMAPCT, GENIE v15 public cohort, and TCGA Pan-Cancer Atlas (32 cohorts)
@@ -198,69 +199,74 @@ def update_vue_counts_json(vues_json, vue_df):
                 effect["counts"] = counts_by_cohort
     return vues_json
 
+cohorts = {
+    "mskimpact": {
+        "clinical_path": "./files/mskimpact/mskimpact_data_clinical_sample.txt",
+        "mutation_path": "./files/mskimpact/mskimpact_data_mutations_extended.txt"
+    },
+    "mskimpact_nonsignedout": {
+        "clinical_path": "./files/mskimpact_nonsignedout/data_clinical_sample.txt",
+        "mutation_path": "./files/mskimpact_nonsignedout/data_nonsignedout_mutations.txt"
+    },
+    "genie": {
+        "clinical_path": "./files/genie/genie_data_clinical_sample.txt",
+        "mutation_path": "./files/genie/genie_data_mutations_extended.txt"
+    }
+}
+# Utility to merge multiple sample maps
+def merge_sample_maps(sample_maps_by_cohort):
+    return dict(chain.from_iterable(d.items() for d in sample_maps_by_cohort.values()))
 
-# Process each cohort
-mskimpact_clinical_df = read_clinical_df("./files/mskimpact/mskimpact_data_clinical_sample.txt", "mskimpact", target_gene_panels_by_cohorts)
-mskimpact_mutation_df = read_mutation_df("./files/mskimpact/mskimpact_data_mutations_extended.txt")
-mskimpact_variant_to_sample, mskimpact_sample_to_mutation_status, mskimpact_sample_to_patient, mskimpact_sample_to_cancer_type = process_cohort(
-    mskimpact_clinical_df,
-    mskimpact_mutation_df,
-    vue_df=vue_df
-)
+# Cohort processing wrapper
+def process_and_count_cohort(name, clinical_path, mutation_path, vue_df, target_gene_panels_by_cohort):
+    clinical_df = read_clinical_df(clinical_path, name, target_gene_panels_by_cohort)
+    mutation_df = read_mutation_df(mutation_path)
+    variant_to_sample, sample_to_mutation_status, sample_to_patient, sample_to_cancer_type = process_cohort(
+        clinical_df, mutation_df, vue_df
+    )
+    vue_df[f'count_{name}'] = add_vue_counts(
+        vue_df=vue_df,
+        variant_to_sample=variant_to_sample,
+        sample_to_mutation_status=sample_to_mutation_status,
+        sample_to_cancer_type=sample_to_cancer_type,
+        sample_to_patient=sample_to_patient,
+        clinical_df=clinical_df
+    )['count']
+    return {
+        "variant_to_sample": variant_to_sample,
+        "sample_to_mutation_status": sample_to_mutation_status,
+        "sample_to_patient": sample_to_patient,
+        "sample_to_cancer_type": sample_to_cancer_type,
+        "clinical_df": clinical_df
+    }
 
-mskimpact_nonsignedout_clinical_df = read_clinical_df("./files/mskimpact_nonsignedout/data_clinical_sample.txt", "mskimpact_nonsignedout", target_gene_panels_by_cohorts)
-mskimpact_nonsignedout_mutation_df = read_mutation_df("./files/mskimpact_nonsignedout/data_nonsignedout_mutations.txt")
-mskimpact_nonsignedout_variant_to_sample, mskimpact_nonsignedout_sample_to_mutation_status, mskimpact_nonsignedout_sample_to_patient, mskimpact_nonsignedout_sample_to_cancer_type = process_cohort(
-    mskimpact_nonsignedout_clinical_df,
-    mskimpact_nonsignedout_mutation_df,
-    vue_df=vue_df
-)
+# Prepare containers
+all_variant_to_sample = {}
+all_sample_to_mutation_status = {}
+all_sample_to_patient = {}
+all_sample_to_cancer_type = {}
+all_clinical_dfs = []
 
-genie_clinical_df = read_clinical_df("./files/genie/genie_data_clinical_sample.txt", "genie", target_gene_panels_by_cohorts)
-genie_mutation_df = read_mutation_df("./files/genie/genie_data_mutations_extended.txt")
-genie_variant_to_sample, genie_sample_to_mutation_status, genie_sample_to_patient, genie_sample_to_cancer_type = process_cohort(
-    genie_clinical_df,
-    genie_mutation_df,
-    vue_df=vue_df
-)
+# Process standard cohorts
+for cohort_name, paths in cohorts.items():
+    result = process_and_count_cohort(
+        name=cohort_name,
+        clinical_path=paths["clinical_path"],
+        mutation_path=paths["mutation_path"],
+        vue_df=vue_df,
+        target_gene_panels_by_cohort=target_gene_panels_by_cohorts
+    )
+    all_variant_to_sample[cohort_name] = result["variant_to_sample"]
+    all_sample_to_mutation_status[cohort_name] = result["sample_to_mutation_status"]
+    all_sample_to_patient[cohort_name] = result["sample_to_patient"]
+    all_sample_to_cancer_type[cohort_name] = result["sample_to_cancer_type"]
+    all_clinical_dfs.append(result["clinical_df"][['PATIENT_ID']])
 
+# Handle TCGA separately
 tcga_clinical_df, tcga_mutation_df = read_all_tcga("./files/tcga")
 tcga_variant_to_sample, tcga_sample_to_mutation_status, tcga_sample_to_patient, tcga_sample_to_cancer_type = process_cohort(
-    tcga_clinical_df,
-    tcga_mutation_df,
-    vue_df=vue_df
+    tcga_clinical_df, tcga_mutation_df, vue_df
 )
-
-# Add counts
-vue_df['count_mskimpact'] = add_vue_counts(
-    vue_df=vue_df,
-    variant_to_sample=mskimpact_variant_to_sample,
-    sample_to_mutation_status=mskimpact_sample_to_mutation_status,
-    sample_to_cancer_type=mskimpact_sample_to_cancer_type,
-    sample_to_patient=mskimpact_sample_to_patient,
-    clinical_df=mskimpact_clinical_df
-    )['count']
-# mskimpact_nonsignedout
-vue_df['count_mskimpact_nonsignedout'] = add_vue_counts(
-    vue_df=vue_df,
-    variant_to_sample=mskimpact_nonsignedout_variant_to_sample,
-    sample_to_mutation_status=mskimpact_nonsignedout_sample_to_mutation_status,
-    sample_to_cancer_type=mskimpact_nonsignedout_sample_to_cancer_type,
-    sample_to_patient=mskimpact_nonsignedout_sample_to_patient,
-    clinical_df=mskimpact_nonsignedout_clinical_df
-)['count']
-
-# genie
-vue_df['count_genie'] = add_vue_counts(
-    vue_df=vue_df,
-    variant_to_sample=genie_variant_to_sample,
-    sample_to_mutation_status=genie_sample_to_mutation_status,
-    sample_to_cancer_type=genie_sample_to_cancer_type,
-    sample_to_patient=genie_sample_to_patient,
-    clinical_df=genie_clinical_df
-)['count']
-
-# tcga
 vue_df['count_tcga'] = add_vue_counts(
     vue_df=vue_df,
     variant_to_sample=tcga_variant_to_sample,
@@ -270,35 +276,14 @@ vue_df['count_tcga'] = add_vue_counts(
     clinical_df=tcga_clinical_df
 )['count']
 
-# Total counts
-all_variant_to_sample = {
-    "mskimpact": mskimpact_variant_to_sample,
-    "mskimpact_nonsignedout": mskimpact_nonsignedout_variant_to_sample,
-    "genie": genie_variant_to_sample,
-    "tcga": tcga_variant_to_sample
-}
+# Include TCGA in total maps
+all_variant_to_sample["tcga"] = tcga_variant_to_sample
+all_sample_to_mutation_status["tcga"] = tcga_sample_to_mutation_status
+all_sample_to_patient["tcga"] = tcga_sample_to_patient
+all_sample_to_cancer_type["tcga"] = tcga_sample_to_cancer_type
+all_clinical_dfs.append(tcga_clinical_df[['PATIENT_ID']])
 
-all_sample_to_mutation_status = {
-    "mskimpact": mskimpact_sample_to_mutation_status,
-    "mskimpact_nonsignedout": mskimpact_nonsignedout_sample_to_mutation_status,
-    "genie": genie_sample_to_mutation_status,
-    "tcga": tcga_sample_to_mutation_status
-}
-
-all_sample_to_patient = {
-    "mskimpact": mskimpact_sample_to_patient,
-    "mskimpact_nonsignedout": mskimpact_nonsignedout_sample_to_patient,
-    "genie": genie_sample_to_patient,
-    "tcga": tcga_sample_to_patient
-}
-
-all_sample_to_cancer_type = {
-    "mskimpact": mskimpact_sample_to_cancer_type,
-    "mskimpact_nonsignedout": mskimpact_nonsignedout_sample_to_cancer_type,
-    "genie": genie_sample_to_cancer_type,
-    "tcga": tcga_sample_to_cancer_type
-}
-
+# Compute total (merged) counts
 total_variant_to_sample = {}
 for cohort_map in all_variant_to_sample.values():
     for vue, samples in cohort_map.items():
@@ -307,12 +292,7 @@ for cohort_map in all_variant_to_sample.values():
 total_sample_to_mutation_status = merge_sample_maps(all_sample_to_mutation_status)
 total_sample_to_patient = merge_sample_maps(all_sample_to_patient)
 total_sample_to_cancer_type = merge_sample_maps(all_sample_to_cancer_type)
-total_clinical_df = pd.concat([
-    mskimpact_clinical_df[['PATIENT_ID']],
-    mskimpact_nonsignedout_clinical_df[['PATIENT_ID']],
-    genie_clinical_df[['PATIENT_ID']],
-    tcga_clinical_df[['PATIENT_ID']]
-], ignore_index=True).drop_duplicates()
+total_clinical_df = pd.concat(all_clinical_dfs, ignore_index=True).drop_duplicates()
 
 vue_df['count_total'] = add_vue_counts(
     vue_df=vue_df,
